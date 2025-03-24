a77456b5b83c38717dfc6d1c067e80c2076e468e
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-03-24 14:18:30 +0700

    menambahkan percobaan tampilan pre test

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("startButton").addEventListener("click", function() {
+    alert("Tes akan segera dimulai!");
+    // Bisa diarahkan ke halaman lain, misalnya: window.location.href = "soal.html";
+});
